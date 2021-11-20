0d3489cdf46d7e8349c865d7d9912bea857c0a4f
Gustavo Cruz <gu.cruz17@hotmail.com>
2021-11-20 01:05:18 -0300

    Alterando retornaTrueSeCpfExiste no FuncionarioDaoTest

--- src/test/java/br/com/projeto/service/dao/FuncionarioDaoTest.java
+++ src/test/java/br/com/projeto/service/dao/FuncionarioDaoTest.java
@@ -64,7 +64,7 @@ public class FuncionarioDaoTest {
 	@Test
 	public void retornaTrueSeCpfExiste() {
 		// Cenario
-		String cpf = "51722662751";
+		String cpf = "51722662759";
 		Funcionario funcionario = new Funcionario(1, "Luciana Clara Bernardes", LocalDate.of(1980, 9, 23), cpf, 'F', "Rua Felicidade, 984 - Rio Branco/AC", "763461", "12345678");
 		funcionarioDao.insert(funcionario);
 		
